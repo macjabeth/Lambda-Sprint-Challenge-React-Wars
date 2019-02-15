44aea591a3fd508f612c2312ced16c82085ce724
macjabeth <macbethjonathan@gmail.com>
2019-02-15 20:30:09 +0000

    complete mvp - add another list element and add more styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './StarWars.css';
 
 const Character = (props) => (
-  <div className="character">
-    <h3>{props.name}</h3>
-  </div>
+  <li className="character">
+    <h3>
+      <span>{props.name}</span>
+      <span>{props.birth_year}</span>
+    </h3>
+  </li>
 );
 
 export default Character;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,11 +3,11 @@ import Character from './Character';
 import './StarWars.css';
 
 const CharacterList = (props) => (
-  <div className="character-list">
+  <ul className="character-list">
     {props.chars.map(char => (
       <Character key={char.name} {...char} />
     ))}
-  </div>
+  </ul>
 );
 
 export default CharacterList;
