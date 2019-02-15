947be604a5d4deef12073d3b95d87dd58e680120
macjabeth <macbethjonathan@gmail.com>
2019-02-15 21:48:07 +0000

    complete stretch task: pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import CharacterList from './components/CharacterList';
+import Loader from 'react-loader-spinner'
 import './App.css';
 
 class App extends Component {
   state = {
-    starwarsChars: []
+    starData: {}
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    setTimeout(() => this.getCharacters(), 3000);
   }
 
-  getCharacters = url => {
+  getCharacters = (url = 'https://swapi.co/api/people') => {
     fetch(url)
-      .then(res => {
-        return res.json();
-      })
+      .then(res => res.json())
       .then(data => {
-        console.log(data.results);
-        this.setState({ starwarsChars: data.results });
+        this.setState({ starData: data });
       })
       .catch(err => {
         throw new Error(err);
@@ -26,13 +24,21 @@ class App extends Component {
   };
 
   render() {
+    console.log(this.state.starData)
+
+    const { results, previous, next } = this.state.starData;
+
     return (
       <React.Fragment>
         <div className="stars"></div>
         <div className="twinkling"></div>
         <div className="app">
-          <h1 className="header">React Wars</h1>
-          <CharacterList chars={this.state.starwarsChars} />
+          <div className="header">
+            {previous ? <span className="prev" onClick={() => this.getCharacters(previous)}>&lt;</span> : null}
+            <h1>React Wars</h1>
+            {next ? <span className="next" onClick={() => this.getCharacters(next)}>&gt;</span> : null}
+          </div>
+          {results ? <CharacterList chars={results} /> : <Loader type="Ball-Triangle" color="red" height={80} width={80} />}
         </div>
       </React.Fragment>
     );
